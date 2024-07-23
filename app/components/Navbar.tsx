4d8b1d2ad2bc7bbd2089bd69fe73444399da4449
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-07-23 17:12:48 +0500

    Finished

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        Marshal<span className="text-primary cursor-pointer">Blog</span>
+        Zaid<span className="text-primary cursor-pointer">Blog</span>
       </Link>
 
       <ModeToggle />
